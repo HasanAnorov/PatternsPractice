e4b4971d0b54158e4223e5a2deb67bf5a0957d25
HasanAnorov <anorovhasan@gmail.com>
2022-03-19 12:00:57 +0500

    Scoped Model Pattern Example & Practice

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
-import '../model/post_model.dart';
-import '../service/network_service.dart';
+import 'package:provider_practice/scopes/home_scoped.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+import '../views/item_of_post.dart';
 
 class HomePage extends StatefulWidget {
   static const id = "home_page";
@@ -12,75 +13,11 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  bool isLoading = false;
-  var items =  <Post>[];
-
-  void  onDelete(Post post){
-    apiDeletePost(post).then((value) => {
-      if(value){
-        _apiPostList(),
-      }
-    });
-  }
-  void onEdit(){}
-
-  Future _apiPostList() async{
-
-    setState(() {
-      isLoading = true;
-    });
-
-    var response = await NetworkService.GET(NetworkService.api_list, NetworkService.paramsEmpty());
-
-    //set state
-    setState(() {
-      if(response !=null){
-        print("response not null");
-        items = NetworkService.parsePostList(response);
-      }
-      isLoading = false;
-    });
-  }
-
-  void _apiUpdatePost(Post post) async {
-
-    setState(() {
-      isLoading = true;
-    });
-
-    var response = await NetworkService.PUT(NetworkService.api_put+post.id.toString(), NetworkService.paramsUpdate(post));
-
-    setState(() {
-      if(response !=null){
-        _apiPostList();
-      }else{
-        items = List<Post>.empty();
-      }
-      isLoading = false;
-    });
-  }
-
-  Future<bool> apiDeletePost(Post post) async{
-
-    isLoading = true;
-
-    var response = await NetworkService.PUT(NetworkService.api_delete+post.id.toString(), NetworkService.paramsEmpty());
-
-    setState(() {
-      if(response != null){
-        _apiPostList();
-      }else{
-        items = List<Post>.empty();
-      }
-      isLoading =false;
-    });
-
-    return response!=null;
-  }
+  HomeScoped scoped = HomeScoped();
 
   @override
   void initState() {
-    _apiPostList();
+    scoped.apiPostList();
     super.initState();
   }
 
@@ -91,19 +28,25 @@ class _HomePageState extends State<HomePage> {
         title: const Text("Provider Practice"),
         centerTitle: true,
       ),
-      body: Stack(
-        children: [
-          ListView.builder(
-            itemBuilder: (ctx,index){
-              return itemOfPost(items[index]);
-            },
-            itemCount: items.length,
-          ),
-          isLoading ? const Center(
-            child: CircularProgressIndicator(),
-          ) :const SizedBox.shrink()
-        ],
-      ),
+      body: ScopedModel<HomeScoped>(
+        model: scoped,
+        child: ScopedModelDescendant<HomeScoped>(
+          builder: (context, child, model) {
+            return Stack(
+              children: [
+                ListView.builder(
+                  itemBuilder: (ctx,index){
+                    return itemOfPost(scoped,scoped.items[index]);
+                  },
+                  itemCount: scoped.items.length,
+                ),
+                scoped.isLoading ? const Center(
+                  child: CircularProgressIndicator(),
+                ) :const SizedBox.shrink()
+              ],
+            );
+          }),
+        ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
         child: const Icon(Icons.add),
@@ -111,56 +54,4 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget itemOfPost( Post post){
-    return Slidable(
-      startActionPane: ActionPane(
-        motion:const ScrollMotion(),
-
-        // dismissible: DismissiblePane(onDismissed: (){
-        //
-        // },),
-
-        children: [
-          SlidableAction(
-            flex: 1,
-            onPressed: (context) => onDelete(post),
-            backgroundColor: const Color(0xFFFE4A49),
-            foregroundColor: Colors.white,
-            icon: Icons.delete,
-            label: 'Delete',
-          ),
-
-        ],
-      ),
-      endActionPane:  ActionPane(
-        motion:const ScrollMotion(),
-        children: [
-          SlidableAction(
-            // An action can be bigger than the others.
-            onPressed:(context) => onEdit(),
-            backgroundColor: const Color(0xFF7BC043),
-            foregroundColor: Colors.white,
-            icon: Icons.archive,
-            label: 'Archive',
-          ),
-        ],
-      ),
-      child: Container(
-        padding: const EdgeInsets.only(left: 20, right: 20, top:20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              post.title.toUpperCase(),
-              style:const TextStyle(
-                  color: Colors.black, fontWeight: FontWeight.bold
-              ),
-            ),
-            const SizedBox(height: 5,),
-            Text(post.body)
-          ],
-        ),
-      ),
-    );
-  }
 }
